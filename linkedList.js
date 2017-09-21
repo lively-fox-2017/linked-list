e46bdfd93932319c064915dfb95850916f6885f6
NNcrawler <rasyid.xyz@gmail.com>
2017-09-21 13:52:27 +0700

    release 1 queue data structure finished

--- linkedList.js
+++ linkedList.js
@@ -1,36 +1,66 @@
 'use strict'
-class singleLinkedList{
-  constructor(){
-    //butuh head sebagai pointer awal
-    this.list=[];
-    this.pointer=-1;
-  }
-  insertLast(element){
-    this.list.push(element);
-    this.pointer++;
-  }
-  deleteLast(){
-    if(this.pointer>=0){
-      this.list.pop();
-      this.pointer--;
-    }
-  }
-  isEmpty(){
-    if(this.pointer==-1){
-      return true;
-    }
-    return false;
-  }
-  displayAll(){
-    console.log(this.list);
+//bentuk linked list object, buat queue
+//require method push, pop,
+// buat satu kelas lagi untuk object yang disimpan
+
+class Node{
+  constructor(value){
+    this.value= value||null;
+    this.next=null;
   }
 }
 
-//tambah
-//hapus
-//is empty
-var testLinkedList = new singleLinkedList();
-testLinkedList.insertLast('HHAHA');
-console.log(testLinkedList.isEmpty());
-testLinkedList.deleteLast();
-testLinkedList.displayAll();
+class Queue{
+  constructor(){
+    this.head=null;
+    this.tail=null;
+  }
+  push(element){
+    //kalo tail head kosong maka inisialisasi
+    //kalo head atau tail ada maka new obj+
+    if(this.head==null){
+      this.head = new Node(element);
+      this.tail = this.head;
+    }else{
+      this.tail.next=new Node(element);
+      this.tail= this.tail.next;
+    }
+  }
+  pop(){
+    if(this.head == null){
+      return null;
+    }else{
+      let nextContainer = this.head.next;
+      let headValue = this.head.value;
+      this.head.next = null;
+      this.head.value = null;
+      this.head = nextContainer;
+      return headValue;
+    }
+  }
+  displayQueue(){
+    let result = '';
+    let node =  this.head;
+    while(node.next!=null){
+      result+=node.value+', ';
+      node = node.next;
+    }
+    result+= node.value;
+
+    return result;
+  }
+}
+
+let testQueue= new Queue();
+console.log(testQueue,'\n');
+testQueue.push('first element');
+console.log(testQueue,'\n');
+testQueue.push('Second element');
+console.log(testQueue, '\n');
+testQueue.push('third element');
+console.log(testQueue);
+console.log('pop value: ',testQueue.pop());
+console.log(testQueue);
+testQueue.push(('fourth element'));
+console.log(testQueue);
+console.log(testQueue.displayQueue());
